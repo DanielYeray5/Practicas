3b5d0396e9f8ac4b337a788f4db14bbc1ae0c801
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-02-22 10:44:23 -0600

    Add files via upload

---
+++ Dif.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int c = 5;
+    printf("c: %d\n", c);
+    printf("c++: %d\n", c++);
+    printf("c: %d\n", c);
+
+    printf("\n");
+
+    int d = 5;
+    printf("c: %d\n", d);
+    printf("++d: %d\n", ++d);
+    printf("d: %d\n", d);
+}
