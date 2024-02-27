59ed38948103125f96024ccf563e69ce58227d91
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-02-27 11:09:43 -0600

    Add files via upload

---
+++ ejemplo2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    //Imprimir el numero 3
+    printf("%d \n",3);
+    //Imprimir la suma de 3 más 4
+    printf("%d \n", 3+4);
+    
+    printf("La suma de %d + %d es = %d\n", 3,4,3+4);
+}
