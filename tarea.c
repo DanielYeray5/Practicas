7f21a166947f25009ec9c409ed7162908c13947a
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-02-28 06:47:46 -0600

    Add files via upload

---
+++ tarea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float a = (1.0/3.0);
+    float b = (3.0/5.0);
+    float c = (1.0/30.0);
+    float r = (23.0/30.0);
+
+    printf("El resultado es: %f", ((a+b+c)/r));
+    return 0;
+}
---
+++ tarea2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("El resultado es: %f ", 5.0 + (2.0/(1.0/(1.0 + (2.0/(2.0-(1.0/4.0)))))));
+    return 0;
+}
