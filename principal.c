25853d18519a25afcd8b5a4b8ba4d72125f8dbc6
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-07 06:45:21 -0600

    Add files via upload

--- principal.c
+++ principal.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    int numero;
-    puts("Ingresa un número");
-    scanf("%i", &numero);
-    int conta = 0;
-    while (conta < numero){
-        if (conta % 3 == 0){
-            printf("Numeros divisibles entre tres dentro de los numeros ingresados %i\n", conta);
-        }
-        conta ++;
+    int arreglo[10];
+    for (int i = 0; i < 10; i++){
+        arreglo[i] = 10;
+        printf("%d \n", arreglo[i]);
     }
+    
     return 0;
 }
+
+/*
+Los valores de Tipo String se tienen que inicializar como si fuera un arreglo
+    char string[] = "Hola mundo";
+    printf("Palabra: %s \n", string);
+    return 0;
+
+Dos (Do while)
+    int i = 0;
+    do{
+        printf("Valor de i = %d \n", i);
+        i++;
+    }while(i < 3);
+
+    puts("Fin");
+
+Tres(Arreglos)
+    int vector[10];
+    int i = 0;
+    while (i < 10){
+        vector[i] = 10;
+        printf("%d \n", vector[i]);
+        i++;
+    }
+*/
