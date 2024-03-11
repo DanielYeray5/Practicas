1af7886f55b741a3497e2cea035b8355e0edd605
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-10 19:31:29 -0600

    Update Ej3.c

--- Ejercicios/Ej3.c
+++ Ejercicios/Ej3.c
@@ -2,11 +2,11 @@
 int main() {
     double precio1, precio2, precio3, media;
 //  Datos
-    printf ("Introduzca el precio en establecimiento 1, en euros: ");
+    printf ("Introduzca el precio en establecimiento 1: ");
     scanf ("%lf", &precio1);
-    printf ("Introduzca el precio en establecimiento 2, en euros: ");
+    printf ("Introduzca el precio en establecimiento 2: ");
     scanf ("%lf", &precio2);
-    printf ("Introduzca el precio en establecimiento 3, en euros: ");
+    printf ("Introduzca el precio en establecimiento 3: ");
     scanf ("%lf", &precio3);
 //Cálculo
     media = (precio1 + precio2 + precio3) / 3;
