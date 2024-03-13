043e937feaf5ccf6429b3bc10c103203203de6fe
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-13 07:37:22 -0600

    Update principal.c

--- Novena/principal.c
+++ Novena/principal.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    int arreglo[100];
+    int i = 0;
+    int contador = 2;
 
+    while (i < 100){
+        arreglo[i] = contador ;
+        printf("%d \n", arreglo[i]);
+        contador +=2;
+        i++;
+    }
     
+    return 0;
 }
