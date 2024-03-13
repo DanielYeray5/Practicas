4e6c8bfdaa749e231790b02f497e0b5868b666dd
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-12 20:51:34 -0600

    Update Ej5.c

---
+++ Ejercicios/Ej5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int segundos, días, horas, minutos;
+
+    printf("Ingrese el número de segundos: ");
+    scanf("%d", &segundos);
+
+    // Calcular el número de días
+    días = segundos / 86400;
+    segundos %= 86400;
+
+    // Calcular el número de horas
+    horas = segundos / 3600;
+    segundos %= 3600;
+
+    // Calcular el número de minutos
+    minutos = segundos / 60;
+    segundos %= 60;
+
+    // Imprimir el número de días, horas, minutos, y segundos
+    printf("%d segundos es igual a %d días, %d horas, %d minutos, y %d segundos\n",
+        segundos, días, horas, minutos, segundos);
+
+    return 0;
+}
