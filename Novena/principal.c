73ea4a40d3b4bee7e3de7190b3b3b137516a596c
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-15 09:10:37 -0600

    Update principal.c

--- Novena/principal.c
+++ Novena/principal.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    int arregloUno[] = {3,5,6,8,4,7,8,5,3,1};
-    int arregloDos[] = {3,4,6,8,9,1,2,3,0,9};
-    int resultado;
+    int a;
+    int i = 0;
+    puts("Ingresa el tamaño del arreglo");
+    scanf("%d", &a);
+    float arreglo[a]; 
+    float sum = 0, product = 1, resultado;
 
-    resultado = arregloUno[3] % (arregloDos[2]/2);
-    printf("El resultado de la operación 1 es: %d\n", resultado);
 
-    resultado = arregloDos[arregloUno[1]] - arregloUno[9];
-    printf("El resultado de la operación 2 es: %d\n", resultado);
-
-    resultado = arregloUno[0] + arregloUno[1 + 2];
-    printf("El resultado de la operación 3 es: %d\n", resultado);
-
-    resultado = arregloUno[5] + arregloDos[5];
-    printf("El resultado de la operación 4 es: %d\n", resultado);
-
-    resultado = (arregloUno[3]/arregloDos[2])/2;
-    printf("El resultado de la operación 5 es: %d\n", resultado);
+    while(i < a){
+        printf("Ingresa el valor del elemento %d:\n ", (i+1));
+        scanf("%f", &arreglo[i]);
+        sum += arreglo[i];
+        product *= arreglo[i]; 
+        i++;
+    }
+    resultado = sum / a;
+    printf("Promedio: %f\n", resultado);
+    printf("Multiplicacion: %f\n",product);
 
     return 0;
 }
@@ -49,7 +49,7 @@ int main(){
     resultado = arregloUno[3] % (arregloDos[2]/2);
     printf("El resultado de la operación 1 es: %d\n", resultado);
 
-    resultado = arregloDos[arregloUno[1]] - arregloUno["9"];
+    resultado = arregloDos[arregloUno[1]] - arregloUno[9];
     printf("El resultado de la operación 2 es: %d\n", resultado);
 
     resultado = arregloUno[0] + arregloUno[1 + 2];
@@ -63,26 +63,4 @@ int main(){
 
     return 0;
 }
-
-
-    int a;
-    int i = 0;
-    puts("Ingresa el tamaño del arreglo");
-    scanf("%d", &a);
-    float arreglo[a]; 
-    float sum = 0, product = 1, resultado;
-
-
-    while(i < a){
-        printf("Ingresa el valor del elemento %d:\n ", (i+1));
-        scanf("%f", &arreglo[i]);
-        sum += arreglo[i];
-        product *= arreglo[i]; 
-        i++;
-    }
-    resultado = sum / a;
-    printf("Promedio: %f\n", resultado);
-    printf("Multiplicacion: %f\n",product);
-
-    return 0;
 */
