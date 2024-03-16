c381e1eaf946ab17a3732017dda7d97f277d48fa
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-16 17:58:44 -0600

    Update Ej6.c

--- Ejercicios/Ej6.c
+++ Ejercicios/Ej6.c
@@ -3,25 +3,25 @@
 int main(){
     int numero;
 
-    printf("\n   Listado de colores de un semaforo:", 160);
-    printf("\n\n   1. Rojo");
-    printf("\n   2. Verde");
-    printf("\n   3. Amarillo");
-    printf("\n\n   Introduzca un color: ", 163);
+    printf("\n Listado de colores de un semaforo:", 160);
+    printf("\n\n 1. Rojo");
+    printf("\n 2. Verde");
+    printf("\n 3. Amarillo");
+    printf("\n\n Introduzca un color: ", 163);
     scanf("%d", &numero);
     switch (numero)
     {
     case 1:
-        printf("\n   Rojo significa: \"No pasar\"");
+        printf("\n Rojo significa: \"No pasar\"");
         break;
     case 2:
-        printf("\n   Verde significa: \"Adelante\"");
+        printf("\n Verde significa: \"Adelante\"");
         break;
     case 3:
-        printf("\n   Amarillo significa: \"Precauci%cn\"", 162);
+        printf("\n Amarillo significa: \"Precauci%cn\"", 162);
         break;
     default:
-        printf("\n   ERROR: %d, no esta asociado a ning%cn color.", numero, 163);
+        printf("\n ERROR: %d, no esta asociado a ningun color.", numero, 163);
     }
 
     return 0;
