9ee2db4d1db36e6ad00b351e3af6a340db0c2716
Daniel Yeray Noguez Iniestra <136346612+DanielYeray5@users.noreply.github.com>
2024-03-19 07:54:57 -0600

    Update Ej7.c

--- Ejercicios/Ej7.c
+++ Ejercicios/Ej7.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    int n1, n2;
+
+    printf( "\n   Introduzca el primer n%cmero (entero): ", 163 );
+    scanf( "%d", &n1 );
+    printf( "\n   Introduzca el segundo n%cmero (entero): ", 163 );
+    scanf( "%d", &n2 );
+
+    if ( n1 > n2 )
+        printf( "\n   El %d es mayor.", n1 );
+    else
+
+        if ( n1 < n2 )
+            printf( "\n   El %d es mayor.", n2 );
+        else
+            printf( "\n   SON IGUALES" );
+
+    getch(); /* Pausa */
+
+    return 0;
 }
